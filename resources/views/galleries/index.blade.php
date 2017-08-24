be8a66df32953beb28e424b8a133ffc8c6f08e00
jambik <jambik@gmail.com>
2017-08-24 16:51:48 +0300

    - first commit

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -6,25 +6,26 @@
 @section('title', '')
 
 @section('content')
-    <h1>Фотогалереи</h1>
-    {!! $page ? $page->text : '' !!}
-    <hr>
-    <div class="galleries-list">
-        @if ($galleries->count())
-            @foreach($galleries as $gallery)
-                <div class="item media category">
-                    <div class="media-left media-middle">
-                        <div class="image"><a href="{{ url('/galleries/' . $gallery->slug) }}"><img src="{{ $gallery->image ? '/images/small/' . $gallery->img_url . $gallery->image : '/img/default.png' }}"></a></div>
+    <div class="container content">
+        <h1>Фотогалереи</h1>
+        {!! $page ? $page->text : '' !!}
+        <hr>
+        <div class="galleries-list">
+            @if ($galleries->count())
+                @foreach($galleries as $gallery)
+                    <div class="item media category">
+                        <div class="media-left media-middle">
+                            <div class="image"><a href="{{ url('/galleries/' . $gallery->slug) }}"><img src="{{ $gallery->image ? '/images/small/' . $gallery->img_url . $gallery->image : '/img/default.png' }}"></a></div>
+                        </div>
+                        <div class="media-body media-middle">
+                            <div class="name"><a href="{{ url('/galleries/' . $gallery->slug) }}">{{ $gallery->name }}</a></div>
+                            @if ($gallery->text)<p>{{ str_limit($gallery->text, 150, '...') }}</p>@endif
+                        </div>
                     </div>
-                    <div class="media-body media-middle">
-                        <div class="name"><a href="{{ url('/galleries/' . $gallery->slug) }}">{{ $gallery->name }}</a></div>
-                        @if ($gallery->text)<p>{{ str_limit($gallery->text, 150, '...') }}</p>@endif
-                    </div>
-                </div>
-            @endforeach
-        @else
-            <div class="no-items">Раздел пока пуст</div>
-        @endif
+                @endforeach
+            @else
+                <div class="no-items">Раздел пока пуст</div>
+            @endif
+        </div>
     </div>
-
 @endsection
