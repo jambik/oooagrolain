17076b54cb5a35b44dff8fea8aaf20c3e5dace54
jambik <jambik@gmail.com>
2017-09-24 16:47:57 +0300

    - first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:100&amp;subset=cyrillic" rel="stylesheet">
     <link href="{{ asset('/css/app.bundle.css') }}" type="text/css" rel="stylesheet">
     <link href="{{ asset('/css/app.css') }}" type="text/css" rel="stylesheet">
     {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
@@ -31,64 +32,177 @@
 <body>
 
 <div class="main-container">
-    <section id="logo">
-        <div class="container">
+
+    <header>
+        <div class="container-fluid header-line-1">
             <div class="row">
                 <div class="col-sm-4">
-                    <div class="logo-text">{!! $settings->description !!}</div>
+                    <div class="menu-button">меню</div>
                 </div>
-                <div class="col-sm-4 logo text-center">
-                    <a href="{{ route('index') }}"><img src="{{ asset('img/logo.png') }}"></a>
+                <div class="col-sm-4 text-center">
+                    <div class="brand-name">ООО Агролайн</div>
                 </div>
-                <div class="col-sm-4 text-right">
-                    <div class="logo-text">Телефон: {!! $settings->phone !!}</div>
+                <div class="col-sm-4 text-right" style="padding-right: 0;">
+                    <div class="stamp-dg"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid header-line-2 text-center">
+            <div class="header-text">
+                Дербент - 300 солнечных дней в году
+            </div>
+            <a href="#" class="header-link">О компании</a>
+        </div>
+    </header>
+
+    <section id="owner">
+        <div class="owner-picture">
+            <img src="{{ asset('img/owner.jpg') }}" alt="">
+        </div>
+    </section>
+
+    <section id="section-first">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="greetings-title">Добро пожаловать на наш сайт</div>
+                    <div class="greetings-line">&nbsp;</div>
+                    <div class="greetings-text">История винодельческого предприятия &laquo;Агролайн&raquo; началась в 2001 году, в удивительном по красоте месте Дагестана, между Каспийским морем и Кавказскими горами. Этот удивительный уголок благодатного края будто самой природой был предназначен для выращивания винограда и виноделия.</div>
+                    <div class="text-center"><img src="http://oooagrolain/files/images/wine-bottles.jpg" style="width: 250px; height: 264px;" /></div>
                 </div>
             </div>
         </div>
     </section>
 
-    @yield('slides')
-
-    <section id="menu">
-        <div class="navbar-header">
-            <div class="navbar-toggle collapsed" data-toggle="collapse" style="float: left; color: #fff; font-size: 16pt; margin: 5px 0 0 15px;">Меню</div>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
-                <span class="sr-only">Меню</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
+    <section id="about">
         <div class="container">
-            <nav class="collapse navbar-collapse" id="navbar">
-                <div class="logo-small">
-                    <!--<div class="brand-name">ООО «Агролайн»</div>
-                    <div class="brand-location">Дербент</div>-->
-                </div>
-                <ul>
-                    <li><a href="/">Главная</a></li>
-                    <li><a href="/page/about">О нас</a></li>
-                    <li><a href="/catalog">Карта вин</a></li>
-                    <li><a href="/galleries">Фотогалерея</a></li>
-                    <li><a href="/page/partners">Партнерам</a></li>
-                    <li><a href="/page/contacts">Контакты</a></li>
-                </ul>
-            </nav>
+            <p>&nbsp;</p>
+            <div class="greetings-title">Полная линия производства вина</div>
+            <p>&nbsp;</p>
+
+            <div class="col-md-3 col-sm-6 about-block">
+                <div class="img"><img class="img-responsive" src="{{ asset('img/vineyard.jpg') }}" /></div>
+                <div class="text-title">Местоположение</div>
+                <p class="about-text">Виноградники винодельческого предприятия &laquo;Агролайн&raquo; расположены в удивительном по красоте месте, между Каспийским морем и Кавказкими горами. Этот удивительный уголок благодатного края будто самой природой был предназначен для выращивания винограда и виноделия.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-6 about-block">
+                <div class="img"><img class="img-responsive" src="{{ asset('img/grapes.jpg') }}" /></div>
+                <div class="text-title">Виноградники</div>
+                <p class="about-text">Собстенные виноградники предприятия занимают площадь в 300 Га, из которых 100 Га являются молодыми. Саженцы отбираются в лучших питомниках как в России, так и в Европе. При этом учитываются такие важные факторы, как структура и текстура почв, особенности рельефа местности.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-6 about-block">
+                <div class="img"><img class="img-responsive" src="{{ asset('img/factory.jpg') }}" /></div>
+                <div class="text-title">Производство</div>
+                <p class="about-text">Производственный комплекс оснащен самым современным оборудованием для переработки сырья и производства вина. На предприятии собрана команда профессионалов своего дела, что позволяет создавать вина с особым &quot;характером&quot; и запоминающимся вкусом.</p>
+            </div>
+
+            <div class="col-md-3 col-sm-6 about-block">
+                <div class="img"><img class="img-responsive" src="{{ asset('img/vine.jpg') }}" /></div>
+                <div class="text-title">Продукция</div>
+                <p class="about-text">&quot;Агролайн&quot; представляет коллекции натуральных виноградных вин в классической форме. Ассортимент вин подобран таким образом, чтобы удовлетворить разнообразные вкусы ценителей вина.</p>
+            </div>
         </div>
+
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
     </section>
+
+    <section id="collection">
+        <div class="container-fluid">
+            <p>&nbsp;</p>
+            <div class="collection-title">Наша коллекция</div>
+            <p>&nbsp;</p>
+
+            <div class="row">
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-1.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Вино «Дербентское»</div>
+                        <div class="type">Сухое, красное</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-red"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-2.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Бланка</div>
+                        <div class="type">Сухое, красное</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-white"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-3.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Вино «Брют»</div>
+                        <div class="type">Сухое, белое</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-red"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-4.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Вино «Дербентское»</div>
+                        <div class="type">Сухое, красное</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-red"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-5.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Бланка</div>
+                        <div class="type">Сухое, красное</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-white"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 collection-item">
+                    <img src="{{ asset('img/product-6.png') }}" alt="">
+                    <div class="details">
+                        <div class="name">Вино «Брют»</div>
+                        <div class="type">Сухое, белое</div>
+                        <div class="description">Букет развитый, на фоне спелых желтых фруктов доминируют яблочные ноты.</div>
+                        <div class="color"><div class="color-red"></div></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+
+        <div>&nbsp;</div>
+
+        <div>&nbsp;</div>
+
+        <div>&nbsp;</div>
+    </section>
+
 
     @yield('content')
 
     <footer>
         <div class="container">
-            <div class="row">
-                <div class="col-xs-6">
-                    <p><img src="{{ asset('img/stamp.png') }}"></p>
-                    {!! $blocks['copyright']->text !!}
-                </div>
-                <div class="col-xs-6 text-right">
-                    <p class="alcohol-note">ЧРЕЗМЕРНОЕ УПОТРЕБЛЕНИЕ АЛКОГОЛЯ ВРЕДИТ ВАШЕМУ ЗДОРОВЬЮ</p>
-                </div>
+            <div class="row text-center">
+                <p class="alcohol-note">ЧРЕЗМЕРНОЕ УПОТРЕБЛЕНИЕ АЛКОГОЛЯ ВРЕДИТ ВАШЕМУ ЗДОРОВЬЮ</p>
+            </div>
+            <div class="row text-center">
+                <img src="{{ asset('img/stamp-dg-small.png') }}" class="stamp-bottom" alt="">
+                <div class="brand-bottom">Агролайн</div>
             </div>
         </div>
     </footer>
